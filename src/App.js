2209082004ae841037092b2f8b8c6a7f9a485e68
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-09-07 00:23:20 +0100

    Added Header Component which contains logo, links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/Shop/ShopPage';
+import Header from './components/Header/Header';
 
  
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
