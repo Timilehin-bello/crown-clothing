242d2731f87b4f792c7cdcdb8219e8cd5a35da54
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-10-25 18:10:17 +0100

    removed white space

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import {store, persistor} from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <BrowserRouter>
-      <PersistGate persistor={persistor} >
+      <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
-
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
